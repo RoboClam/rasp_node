534b7ca0b9d2959aa940c160dcce49eaf214913a
jacobwright <jacob.thewrightone@gmail.com>
2021-06-05 13:57:51 -0500

    Styling changes

--- default.js
+++ default.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.get('/', function (req, res) { res.send('Hello World!')});
 
-app.listen(7777, function() { console.log('Example app listening on port 3010!'); });
+app.listen(7777, function() { console.log('Example app listening on port 7777!'); });
