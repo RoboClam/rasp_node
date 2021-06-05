a11463643dbc337e692b6ef232ec5d1105c3e7b3
jacobwright <jacob.thewrightone@gmail.com>
2021-06-04 20:51:17 -0500

    Added relay and client example html

--- websocket.js
+++ websocket.js
@@ -3,9 +3,14 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 7777 });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+  ws.on('message', function incoming(data) {
+    console.log('received: %s', data);
+    wss.clients.forEach(function each(client) { // Relay incoming message to all clients EXCLUDING self
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
   });
 
-  ws.send('something');
+  ws.send('You are now connected to the network! Welcome Aboard.');
 });
