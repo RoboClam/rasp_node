a5a42ea01994b0ae00d04469f2f5a23581a3e440
jacobwright <jacob.thewrightone@gmail.com>
2021-06-04 21:43:11 -0500

    fixed message data message

--- websocket.js
+++ websocket.js
@@ -9,9 +9,9 @@ wss.on('connection', function connection(ws, req) {
   	console.log(ip.split(',')[0].trim());
   ws.on('message', function incoming(incoming) {
 	let parsed = JSON.parse(incoming);
-	let data = parsed.data;
+	let data = parsed.message;
 	let who = parsed.who;
-	console.log('received: %s', data);
+	console.log('%s says: %s', who, data);
     wss.clients.forEach(function each(client) { // Relay incoming message to all clients EXCLUDING self
       if (client !== ws && client.readyState === WebSocket.OPEN) {
 		  let packet = JSON.stringify({'who' : who, 'message' : data});
