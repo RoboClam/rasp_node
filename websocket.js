c91f646189e0ed7a59d4574649ccb769efb5b37d
jacobwright <jacob.thewrightone@gmail.com>
2021-06-04 21:22:47 -0500

    ip info

--- websocket.js
+++ websocket.js
@@ -2,12 +2,13 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 7777 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, req) {
+  const ip = req.headers['x-forwarded-for'].split(',')[0].trim();
   ws.on('message', function incoming(data) {
     console.log('received: %s', data);
     wss.clients.forEach(function each(client) { // Relay incoming message to all clients EXCLUDING self
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
+        client.send({'who' : ip, 'message' : data});
       }
     });
   });
