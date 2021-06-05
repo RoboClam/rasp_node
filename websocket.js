5af1697fcd62d5fd37a43cb6e065d1e00eed9892
jacobwright <jacob.thewrightone@gmail.com>
2021-06-04 21:47:04 -0500

    whitespace cleanup

--- websocket.js
+++ websocket.js
@@ -3,14 +3,10 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 7777 });
 
 wss.on('connection', function connection(ws, req) {
-  let ip = req.headers['x-forwarded-for'];
-  console.log(ip);
-  if(ip)
-  	console.log(ip.split(',')[0].trim());
   ws.on('message', function incoming(incoming) {
 	let parsed = JSON.parse(incoming);
 	let data = parsed.message;
-	let who = parsed.who !== "" ? parsed.who : "unknown";
+	let who = parsed.who.trim() !== "" ? parsed.who.trim() : "unknown";
 	console.log('%s says: %s', who, data);
     wss.clients.forEach(function each(client) { // Relay incoming message to all clients EXCLUDING self
       if (client !== ws && client.readyState === WebSocket.OPEN) {
