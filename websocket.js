dd3e09e4b2005fed6d119c547151c3df8560f7bb
jacobwright <jacob.thewrightone@gmail.com>
2021-06-04 21:39:52 -0500

    who

--- websocket.js
+++ websocket.js
@@ -7,11 +7,14 @@ wss.on('connection', function connection(ws, req) {
   console.log(ip);
   if(ip)
   	console.log(ip.split(',')[0].trim());
-  ws.on('message', function incoming(data) {
-    console.log('received: %s', data);
+  ws.on('message', function incoming(incoming) {
+	let parsed = JSON.parse(incoming);
+	let data = parsed.data;
+	let who = parsed.who;
+	console.log('received: %s', data);
     wss.clients.forEach(function each(client) { // Relay incoming message to all clients EXCLUDING self
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-		  let packet = JSON.stringify({'who' : ip ? ip : "unknown", 'message' : data});
+		  let packet = JSON.stringify({'who' : who ? who : "unknown", 'message' : data});
         client.send(packet);
       }
     });
