19945b2d92e64333034544ea4fe7a834c77cc36d
Roboclam <jacob.thewrightone@gmail.com>
2021-06-05 22:11:39 -0500

    Unity position test

--- websocket.js
+++ websocket.js
@@ -5,20 +5,34 @@ const wss = new WebSocket.Server({ port: 7777 });
 wss.on('connection', function connection(ws, req) {
   console.log("New Connection...");
   ws.on('message', function incoming(incoming) {
-	let parsed = JSON.parse(incoming);
-	let data = parsed.message;
-	let who = parsed.who.trim() !== "" ? parsed.who.trim() : "unknown";
-	console.log('%s says: %s', who, data);
-    wss.clients.forEach(function each(client) { // Relay incoming message to all clients EXCLUDING self
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-		  let packet = JSON.stringify({'who' : who, 'message' : data});
-        client.send(packet);
-      }
-    });
+    let parsed = JSON.parse(incoming);
+
+    if (parsed.type === 'CHAT') {
+      let data = parsed.message;
+      let who = parsed.who.trim() !== "" ? parsed.who.trim() : "unknown";
+      console.log('%s says: %s', who, data);
+      wss.clients.forEach(function each(client) { // Relay incoming message to all clients 
+        if (client !== ws && client.readyState === WebSocket.OPEN) { // EXCLUDING self
+          let packet = JSON.stringify({ 'who': who, 'message': data });
+          client.send(packet);
+        }
+      });
+    }
+
+    if (parsed.type === 'UNITY') {
+      let who = parsed.who.trim() !== "" ? parsed.who.trim() : "unknown";
+      console.log("Unity scene updates..." + parsed);
+      
+    }
+
   });
 
   ws.send('You are now connected to the network! Welcome Aboard.');
-  setInterval( () => {
-    ws.send("Hello?");
+
+
+  let xx, yy, zz = 0;
+  setInterval(() => {
+    let unityPacket = JSON.stringify({'who': 'someone', 'xx' : xx++, 'yy' : yy++, 'zz' : zz++});
+    ws.send(unityPacket);
   }, 1700);
 });
